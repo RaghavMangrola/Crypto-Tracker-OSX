8486007f2089ecf9d2c8627ece4919f8c2116fa3
Raghav Mangrola <raghav.mangrola@eventfarm.com>
2018-03-05 10:42:15 -0800

    Initial Commit

---
+++ XRP Tracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XRP Tracker
+//
+//  Created by Raghav Mangrola on 3/5/18.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
